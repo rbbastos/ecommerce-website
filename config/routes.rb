d430943f89ee520d653d23ffe0bf99c74fa3ffe9
rbbastos <bastoscanada@gmail.com>
2019-11-01 20:43:40 -0500

    Added Province controller action index + routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # get 'products/show'
 
   resources :products, only: %i[index show]
+
+  resources :provinces, only: :index
+  # get 'provinces/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
 end
