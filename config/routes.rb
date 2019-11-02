ef0c14905a39c1eba726b456497729dbc47207de
rbbastos <bastoscanada@gmail.com>
2019-11-01 20:38:48 -0500

    Added Products controller index show + routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@
 Rails.application.routes.draw do
   # get 'categories/index'
   resources :categories, only: %i[index show]
+
+  # get 'products/index'
+  # get 'products/show'
+
+  resources :products, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
 end
