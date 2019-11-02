f3d2439c3e8109d69a88177fd567895c57e1b4ed
rbbastos <bastoscanada@gmail.com>
2019-11-01 20:36:02 -0500

    Added Category controller + view + partial for category + routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(:name)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # get 'categories/index'
+  resources :categories, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'products#index'
 end
