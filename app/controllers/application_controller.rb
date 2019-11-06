79896488e216082e7845ff3cbb10179c7c00347b
rbbastos <bastoscanada@gmail.com>
2019-11-06 09:03:43 -0600

    Solved Conflicts in merged files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
-<<<<<<< Updated upstream
-class ApplicationController < ActionController::Base
-end
-=======
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pagy::Backend
 end
->>>>>>> Stashed changes
