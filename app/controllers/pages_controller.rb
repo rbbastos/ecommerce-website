5329e61df6491d5ff7b24b0e690ccbe8e9a7f617
rbbastos <bastoscanada@gmail.com>
2019-11-15 15:43:25 -0600

    Implemented permalink action, route + basic authentication for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
+
 class PagesController < InheritedResources::Base
+  http_basic_authenticate_with name: 'rbbastos', password: '1234'
 
-  private
+  def page_params
+    params.require(:page).permit(:title, :content, :permalink)
+  end
 
-    def page_params
-      params.require(:page).permit(:title, :content, :permalink)
+  def permalink
+    @page = Page.find_by_permalink(params[:permalink])
+
+    if @page
+      # test if find_by_permalink worked properly
+      render :show # /app/views/pages/show.html.erb
+    else
+      # grab error in find_by_permalink
+      redirect_to root_path
     end
-
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   resources :pages
+
+  get '/static/:permalink', to: 'pages#permalink', as: 'permalink'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # get 'categories/index'
