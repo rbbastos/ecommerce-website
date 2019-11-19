56a0b937816fec77320a5ea26f5f0e7a82f5d951
rbbastos <bastoscanada@gmail.com>
2019-11-19 11:12:21 -0600

    Fixed lineItems to line_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_one :payment
-  has_many :lineItems
-  has_many :products, through: :lineItems, source: :product
+  has_many :line_items
+  has_many :products, through: :line_items, source: :product
   validates :pstTimeOfPurchase, :gstTimeOfPurchase, presence: true
   validates :pstTimeOfPurchase, :gstTimeOfPurchase, numericality: { only_decimal: true }
 end
